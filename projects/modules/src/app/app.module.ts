2d066fa789e52483cbd3f8096f8d755d18c4b9b5
Stéphane Francel <stephane.francel@gmail.com>
2024-03-04 17:04:39 +0100

    Components : projection

--- projects/modules/src/app/app.module.ts
+++ projects/modules/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ModalComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
